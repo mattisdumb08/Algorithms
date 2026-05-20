ffa4499cbae841897c572ec1090384d516bcc527
Matt <91633363+mattisdumb08@users.noreply.github.com>
2026-05-20 10:56:48 +0000

    Converted the array to a struct and functions to go with it

--- hashTable.c
+++ hashTable.c
@@ -5,7 +5,7 @@ struct integerHashTable {
 
     int maxSize;
     int size;
-    int hashTable[1];
+    int* hashTable; // Pointer to an array
 
 };
 
@@ -17,50 +17,64 @@ int hash(int value , int maxSize){
     
 }
 
-void output(int table[] , int maxSize){
+void outputIntHashTable(struct integerHashTable table){
     
-    for (int i = 0 ; i < maxSize ; i++){
+    for (int i = 0 ; i < table.maxSize ; i++){
         
-        printf("%i\n" , table[i]);
+        printf("%i\n" , table.hashTable[i]);
         
     }
     
 }
 
-void push(int input , int maxSize , int hashTable[]){
+void pushIntHashTable(int input , struct integerHashTable hashTable){
     
-    int hashedAddress = hash(input , maxSize);
+    int hashedAddress = hash(input , hashTable.maxSize);
     
-    if (hashTable[hashedAddress] == 0){
-        hashTable[hashedAddress] = input;
-    } else if (hashTable[hashedAddress] != 0){
+    if (hashTable.hashTable[hashedAddress] == 0){
+        hashTable.hashTable[hashedAddress] = input;
+    } else if (hashTable.hashTable[hashedAddress] != 0){
         
-        while (hashTable[hashedAddress] != 0){
-            hashedAddress = (hashedAddress + 3) % maxSize;
+        while (hashTable.hashTable[hashedAddress] != 0){
+            hashedAddress = (hashedAddress + 3) % hashTable.maxSize;
         }
         
-        hashTable[hashedAddress] = input;
+        hashTable.hashTable[hashedAddress] = input;
         
     }
+}
+
+struct integerHashTable newIntegerHashTable(int maxSize){
+
+    struct integerHashTable newTable;
+
+    newTable.size = 0;
+    newTable.maxSize = maxSize;
+    newTable.hashTable = malloc(sizeof(int)  * maxSize);
+
+    for (int i = 0 ; i < newTable.maxSize ; i++){
+        newTable.hashTable[i] = 0;
+    }
+
+    return newTable;
+
 }
 
 int main() {
     
     int maxSize = 5;
     
-    int hashTable[maxSize];
-    
-    for (int i = 0 ; i < maxSize ; i++){
-        hashTable[i] = 0;
-    }
+    struct integerHashTable hashTable = newIntegerHashTable(maxSize);
     
     int input = 4;
     
-    push(input , maxSize , hashTable);
-    push(94 , maxSize , hashTable);
-    push(55 , maxSize , hashTable);
+    pushIntHashTable(input , hashTable);
+    pushIntHashTable(94  , hashTable);
+    pushIntHashTable(55  , hashTable);
     
-    output(hashTable , maxSize);
+    outputIntHashTable(hashTable);
+
+    free(hashTable.hashTable);
 
     return 0;
 }
