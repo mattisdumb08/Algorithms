050f4b59861f2cc8ec886720bf1f1ff54277b6b8
Matt <91633363+mattisdumb08@users.noreply.github.com>
2026-05-21 11:04:46 +0000

    Added guard clause

--- hashTable.c
+++ hashTable.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h> // In the case that I ever need dynamic memory ( for some odd reasons )
+#include <stdlib.h> // In the case that I ever need dynamic memory
 
 struct integerHashTable {
 
@@ -36,17 +36,45 @@ void pushIntHashTable(int input , struct integerHashTable hashTable){
     
     int hashedAddress = hash(input , hashTable.maxSize);
     
+    if (hashTable.size >= hashTable.maxSize){
+        printf("Error: Max capacity of the hash table has been reached");
+        return;
+    }
+
     if (hashTable.hashTable[hashedAddress] == 0){
         hashTable.hashTable[hashedAddress] = input;
+        hashTable.size = hashTable.size + 1;
     } else if (hashTable.hashTable[hashedAddress] != 0){
+
+        unsigned short int iterations = 0;
+
+        int previousAddress = hashedAddress;
+
+        short int addressFound = 0;
         
         while (hashTable.hashTable[hashedAddress] != 0){
             hashedAddress = (hashedAddress + 3) % hashTable.maxSize;
+
+            if (hashedAddress < previousAddress){
+                iterations += 1;
+            }
+
+            if (iterations >= 4){
+                break;
+            }
+
+            previousAddress = hashedAddress;
+            addressFound = 1;
         }
         
-        hashTable.hashTable[hashedAddress] = input;
+        if (addressFound == 1){
+            hashTable.hashTable[hashedAddress] = input;
+            hashTable.size = hashTable.size + 1;
+        }
         
     }
+
+    return;
 }
 
 // Initialiser for the IntegerHashTable struct
@@ -79,7 +107,9 @@ int main() {
     pushIntHashTable(55  , hashTable);
     pushIntHashTable(87 , hashTable);
     pushIntHashTable(634 , hashTable);
-    
+    pushIntHashTable(999 , hashTable);
+    printf("%i\n----------\n" , hashTable.size);
+
     outputIntHashTable(hashTable);
 
     // Frees the memory of the malloc in the struct
